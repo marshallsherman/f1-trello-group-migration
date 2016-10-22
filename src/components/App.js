3667085e454701e8abcba84cc73daa8a39735eb3
Cameron Scroggins <cameron.scroggins@gmail.com>
2016-10-22 15:23:40 -0500

    Remove a bunch of demo app stuff, init app with nav bar.

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,23 @@
 import React, { PropTypes } from 'react';
-import { Link, IndexLink } from 'react-router';
+// import { Link, IndexLink } from 'react-router';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from './AppBar';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-const App = (props) => {
+const App = () => {
   return (
-    <div>
-      <IndexLink to="/">Home</IndexLink>
-      {' | '}
-      <Link to="/fuel-savings">Demo App</Link>
-      {' | '}
-      <Link to="/about">About</Link>
-      <br/>
-      {props.children}
-    </div>
+    <MuiThemeProvider>
+      <AppBar />
+    </MuiThemeProvider>
   );
 };
 
 App.propTypes = {
   children: PropTypes.element
 };
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 export default App;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import {Link} from 'react-router';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Card, CardActions} from 'material-ui/card/card';
 
-const HomePage = () => {
-  return (
-    <section class="flex">
-      <div>
-        <h1>Twelve</h1>
-      </div>
 
-      <div class="selection">
-        <select class="dropdown-menu">Choose Option
-          <option>Option 1</option>
-          <option>Option 2</option>
-          <option>Option 3</option>
-        </select>
-      </div>
-    </section>
-  );
-};
+// const HomePage = () => {
+//   return (
+    
+//   );
+// };
 
-export default HomePage;
+const CardExampleWithAvatar = () => (
+  <Card>
+    <CardHeader
+      title="URL Avatar"
+      subtitle="Subtitle"
+      avatar="http://lorempixel.com/100/100/nature/"
+    />
+    <CardMedia
+      overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+    >
+      <img src="http://lorempixel.com/600/337/nature/" />
+    </CardMedia>
+    <CardTitle title="Card title" subtitle="Card subtitle" />
+    <CardText>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+    </CardText>
+    <CardActions>
+      <FlatButton label="Action1" />
+      <FlatButton label="Action2" />
+    </CardActions>
+  </Card>
+);
+
+export default CardExampleWithAvatar;
